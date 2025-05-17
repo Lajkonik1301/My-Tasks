b91d430b4e3ba69bf40fdc5533e515b3109701f4
Wiktor Koźlik <wiktor.kozlik1@gmail.com>
2025-05-17 17:07:03 +0200

    login window - done

--- My-Tasks/LoginWindow.cpp
+++ My-Tasks/LoginWindow.cpp
@@ -12,22 +12,54 @@ EVT_BUTTON(ID_LOGIN, LoginWindow::OnLogin)
 EVT_BUTTON(ID_REGISTER, LoginWindow::OnRegister)
 wxEND_EVENT_TABLE()
 
-LoginWindow::LoginWindow()
-    : wxFrame(nullptr, wxID_ANY, "My Tasks", wxDefaultPosition, wxSize(300, 300)) {
+LoginWindow::LoginWindow() : wxFrame(nullptr, wxID_ANY, "My Tasks", wxDefaultPosition, wxSize(280, 340)) {
+
+    wxFont headlineFont(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+
     wxPanel* panel = new wxPanel(this);
 
-    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-    sizer->Add(new wxStaticText(panel, wxID_ANY, "Login:"), 0, wxALL, 5);
-    loginCtrl = new wxTextCtrl(panel, wxID_ANY);
-    sizer->Add(loginCtrl, 0, wxALL | wxEXPAND, 5);
-    sizer->Add(new wxStaticText(panel, wxID_ANY, "Hasło:"), 0, wxALL, 5);
-    passCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD);
-    sizer->Add(passCtrl, 0, wxALL | wxEXPAND, 5);
+    wxStaticText* headlineText = new wxStaticText(panel, wxID_ANY, "My Tasks");
+    headlineText->SetFont(headlineFont);
 
-    sizer->Add(new wxButton(panel, ID_LOGIN, "Zaloguj się"), 0, wxALL | wxEXPAND, 5);
-    sizer->Add(new wxButton(panel, ID_REGISTER, "Zarejestruj się"), 0, wxALL | wxEXPAND, 5);
+    wxStaticText* usernameLabel = new wxStaticText(panel, wxID_ANY, "Login:");
+    wxTextCtrl* usernameCtrl = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(150, -1));
 
-    panel->SetSizer(sizer);
+    wxStaticText* passwordLabel = new wxStaticText(panel, wxID_ANY, "Hasło:");
+    wxTextCtrl* passwordCtrl = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(150, -1), wxTE_PASSWORD);
+
+    wxButton* loginBtn = new wxButton(panel, ID_LOGIN, "Zaloguj się", wxDefaultPosition, wxSize(150, -1));
+    wxButton* registerBtn = new wxButton(panel, ID_REGISTER, "Zarejestruj się", wxDefaultPosition, wxSize(150, -1));
+
+    usernameCtrl->SetFocus();
+
+    // Układ
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+
+    mainSizer->Add(headlineText, wxSizerFlags().CenterHorizontal());
+    mainSizer->AddSpacer(15);
+
+    wxBoxSizer* usernameSizer = new wxBoxSizer(wxVERTICAL);
+    usernameSizer->Add(usernameLabel, wxSizerFlags().Align(wxALIGN_LEFT));
+    usernameSizer->AddSpacer(4);
+    usernameSizer->Add(usernameCtrl, wxSizerFlags().CenterHorizontal());
+    mainSizer->Add(usernameSizer, wxSizerFlags().CenterHorizontal());
+    mainSizer->AddSpacer(15);
+
+    wxBoxSizer* passwordSizer = new wxBoxSizer(wxVERTICAL);
+    passwordSizer->Add(passwordLabel, wxSizerFlags().Align(wxALIGN_LEFT));
+    passwordSizer->AddSpacer(4);
+    passwordSizer->Add(passwordCtrl, wxSizerFlags().CenterHorizontal());
+    mainSizer->Add(passwordSizer, wxSizerFlags().CenterHorizontal());
+    mainSizer->AddSpacer(15);
+
+    mainSizer->Add(loginBtn, wxSizerFlags().CenterHorizontal());
+    mainSizer->AddSpacer(5);
+    mainSizer->Add(registerBtn, wxSizerFlags().CenterHorizontal());
+
+    wxGridSizer* outerSizer = new wxGridSizer(1);
+    outerSizer->Add(mainSizer, wxSizerFlags().Border(wxALL, 25).Expand());
+
+    panel->SetSizer(outerSizer);
     Centre();
 }
 
